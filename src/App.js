d060e1e8b72dbd544b7d680aac65b521acffd149
Umar Jamil <99021340+UMAR-78@users.noreply.github.com>
2024-01-15 19:10:25 +0500

    Add Favicon Icon and Update Tittle

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-        <Footer/>
+        {/* <Footer/> */}
       </Router>
     </>
   );
--- src/components/ToolsAndTechnologies/Tools.jsx
+++ src/components/ToolsAndTechnologies/Tools.jsx
@@ -1,6 +1,63 @@
 import React from "react";
 import "./Tools.css";
 
+const toolsData = [
+  {
+    imageSrc: "Brand=figma, Style=Light.svg",
+    heading: "Figma",
+    description:
+      "I design user interfaces collaboratively using Figma, streamlining teamwork.",
+  },
+  {
+    imageSrc: "githubDark.png",
+    heading: "Github",
+    description:
+      "I leverage GitHub for efficient version control and collaborative software development.",
+  },
+  {
+    imageSrc: "Brand=html5, Style=Light.svg",
+    heading: "HTML 5",
+    description:
+      "I implement HTML5 for structured and enhanced web content presentation.",
+  },
+  {
+    imageSrc: "Brand=tailwind, Style=Light.png",
+    heading: "Tailwind",
+    description:
+      "I optimize styling with Tailwind CSS, embracing its utility-first approach to effortlessly create and customize styles in web development.",
+  },
+  {
+    imageSrc: "Brand=redux, Style=Light.png",
+    heading: "Redux Toolkit",
+    description:
+      "I implement Redux Toolkit to streamline state management in React applications, ensuring efficiency and maintainability.",
+  },
+  {
+    imageSrc: "Brand=reactjs, Style=Light.svg",
+    heading: "React js",
+    description:
+      "I employ ReactJS for dynamic and interactive user interfaces in web applications.",
+  },
+  {
+    imageSrc: "Brand=postman, Style=Light.png",
+    heading: "Postman",
+    description:
+      "I depend on Postman for efficient API testing and development, fostering collaboration and ensuring robust testing.",
+  },
+  {
+    imageSrc: "Brand=nodejs, Style=Light.svg",
+    heading: "Node js",
+    description:
+      "I use Node.js to build scalable and high-performance network applications.",
+  },
+  {
+    imageSrc: "Brand=mongodb, Style=Light.png",
+    heading: "Mongodb",
+    description:
+      "I choose MongoDB for flexible and scalable NoSQL database solutions in seamless data storage and retrieval.",
+  },
+];
+
 const Tools = () => {
   return (
     <div>
@@ -12,68 +69,13 @@ const Tools = () => {
         </p>
       </div>
       <div className="ToolsDiv">
-        <div className="tool">
-          <img className="toolsImage" src="Brand=figma, Style=Light.svg" alt="" />
-          <h5 className="toolHeading">Figma</h5>
-          <p className="toolDescription">
-            I design user interfaces collaboratively using Figma, streamlining
-            teamwork.
-          </p>
-        </div>
-        <div className="tool">
-          <img className="toolsImage" src="githubDark.png" alt="" />
-          <h5 className="toolHeading">Github</h5>
-          <p className="toolDescription">
-          I leverage GitHub for efficient version control and collaborative software development.
-          </p>
-        </div>
-        <div className="tool">
-          <img className="toolsImage" src="Brand=html5, Style=Light.svg" alt="" />
-          <h5 className="toolHeading">HTML 5</h5>
-          <p className="toolDescription">
-          I implement HTML5 for structured and enhanced web content presentation.
-          </p>
-        </div>
-        <div className="tool">
-          <img className="toolsImage" src="Brand=tailwind, Style=Light.png" alt="" />
-          <h5 className="toolHeading">Tailwind</h5>
-          <p className="toolDescription">
-          I optimize styling with Tailwind CSS, embracing its utility-first approach to effortlessly create and customize styles in web development.
-          </p>
-        </div>
-        <div className="tool">
-          <img className="toolsImage" src="Brand=redux, Style=Light.png" alt="" />
-          <h5 className="toolHeading">Redux Toolkit</h5>
-          <p className="toolDescription">
-          I implement Redux Toolkit to streamline state management in React applications, ensuring efficiency and maintainability.
-          </p>
-        </div>
-        <div className="tool">
-          <img className="toolsImage" src="Brand=reactjs, Style=Light.svg" alt="" />
-          <h5 className="toolHeading">React js</h5>
-          <p className="toolDescription">
-          I employ ReactJS for dynamic and interactive user interfaces in web applications.
-          </p>
-        </div>
-        <div className="tool">
-          <img className="toolsImage" src="Brand=postman, Style=Light.png" alt="" />
-          <h5 className="toolHeading">Postman</h5>
-          <p className="toolDescription">
-          I depend on Postman for efficient API testing and development, fostering collaboration and ensuring robust testing</p>
-        </div>
-        <div className="tool">
-          <img className="toolsImage" src="Brand=nodejs, Style=Light.svg" alt="" />
-          <h5 className="toolHeading">Node js</h5>
-          <p className="toolDescription">
-          I use Node.js to build scalable and high-performance network applications.
-          </p>
-        </div>
-        <div className="tool">
-          <img className="toolsImage" src="Brand=mongodb, Style=Light.png" alt="" />
-          <h5 className="toolHeading">Mongodb</h5>
-          <p className="toolDescription">
-          I choose MongoDB for flexible and scalable NoSQL database solutions in seamless data storage and retrieval.</p>
-        </div>
+        {toolsData.map((tool, index) => (
+          <div key={index} className="tool">
+            <img className="toolsImage" src={tool.imageSrc} alt="" />
+            <h5 className="toolHeading">{tool.heading}</h5>
+            <p className="toolDescription">{tool.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
