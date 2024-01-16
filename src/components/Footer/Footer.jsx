d2e384d5f0af2355318c4839fe48bda2df2a0fd9
Umar Jamil <99021340+UMAR-78@users.noreply.github.com>
2024-01-16 23:49:13 +0500

    Add Foote Section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,40 +1,15 @@
-import React from "react";
-import "./Footer.css";
+import React from 'react'
+import "./Footer.css"
 
 const Footer = () => {
-  return (
-    <div className="footerSection">
-      <div className="footer">
-        <div>
-          <h3 className="logoHeading">Umar Jamil</h3>
-        </div>
-        <div></div>
-        <div className="CLASS">
-          <h4>Social Platforms</h4>
-          <ul>
-            <li>
-              {" "}
-              <a href="">Instagram</a>
-            </li>
-            <li>
-              <a href="">Gitub</a>
-            </li>
-            <li>
-              <a href="">LinkedIn</a>
-            </li>
-          </ul>
-        </div>
-        <div className="CLASS">
-          <h4>Quick Contact</h4>
-          <ul>
-            <li>Instagram</li>
-            <li>Gitub</li>
-            <li>LinkedIn</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
-};
+  const currentYear = new Date().getFullYear();
 
-export default Footer;
+  return (
+    <div className='Footer'>
+      <div className='spanLineforFooter'></div>
+      <p className='copyrightclass'>Design & Built by Umar Jamil &copy; {currentYear}. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
