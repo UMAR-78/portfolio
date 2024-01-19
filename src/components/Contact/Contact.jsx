b3d230a5481ca202def05b3eb2228aad5088ae3f
Umar Jamil <99021340+UMAR-78@users.noreply.github.com>
2024-01-20 00:21:10 +0500

    Add Contact Page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import "./Contact.css";
+
+// import React from "react";
 
 const Contact = () => {
-  return (
-    <div className="container2 ContactSection">
-      <h1 className="contentHeading">
-        <span>06. </span>Contact me <span className="spanLine"></span> <br />
-        Coming soon.....
-      </h1>
-    </div>
-  )
-}
+  const [state, handleSubmit] = useForm("mbjnlvbb");
 
-export default Contact
+  if (state.succeeded) {
+    return <p className="successMessage">Thanks for submitting the form!</p>;
+  }
+  return (
+    <div className="container2 contactSection">
+      <div>
+        <h1 className="contentHeading">
+          <span>06. </span>Contact Me <span className="spanLine"></span>
+        </h1>
+      </div>
+      <form onSubmit={handleSubmit} className="contactForm">
+        <label htmlFor="email">Email Address</label>
+        <input id="email" type="email" name="email" required />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+
+        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message" required />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+
+        <button type="submit" className="button" disabled={state.submitting}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import './HeroSection.css'
+import React from "react";
+import "./HeroSection.css";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
-    <div className='container HeroSection'>
-      <h4 className='firstText'>Hi, my name is</h4>
-      <h1 className='secondText'>Umar Jamil</h1>
-      <h1 className='thirdText'>I build things for the web.</h1>
-      <p className='text'>A dedicated software engineer with a primary focus on web development, particularly <br /> in the frontend. I excel in crafting engaging and <span>user-friendly web interfaces</span>, using<br /> my skills to enhance the online experience through intuitive design and cutting-edge<br /> technologies. Let's bring your web projects to life with creativity and precision.</p>
-      <button type='button' className='button'>Lets Discuss</button>
-    </div>
-  )
-}
+    <div className="container HeroSection">
+      <h4 className="firstText">Hi, my name is</h4>
+      <h1 className="secondText">Umar Jamil</h1>
+      <h1 className="thirdText">I build things for the web.</h1>
+      <p className="text">
+        A dedicated software engineer with a primary focus on web development,
+        particularly <br /> in the frontend. I excel in crafting engaging and{" "}
+        <span>user-friendly web interfaces</span>, using
+        <br /> my skills to enhance the online experience through intuitive
+        design and cutting-edge
+        <br /> technologies. Let's bring your web projects to life with
+        creativity and precision.
+      </p>
 
-export default HeroSection
+      <Link to={"/contact"}>
+        <button type="button" className="button">
+          Lets Discuss
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default HeroSection;
